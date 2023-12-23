63d28d139673796873544d68bf1a9bdf0e697770
Jinho Kim <wlsghrla94@gmail.com>
2023-12-23 16:20:17 +0900

    Test middleware

--- server.js
+++ server.js
@@ -1,31 +1,28 @@
 const express = require("express");
-const { Pool } = require("pg");
 
 const app = express();
 const port = 8080;
-
-const pool = new Pool({
-  user: "jinho",
-  host: "localhost",
-  database: "goldmoon",
-  password: "postgres",
-  port: 5432,
+app.use(express.json());
+const data = [
+  { id: "id-1", name: "name-1", note: "note info 1" },
+  { id: "id-2", name: "name-2", note: "note info 2" },
+  { id: "id-3", name: "name-3", note: "note info 3" },
+];
+app.get("/notes", async (req, res) => {
+  res.send(data);
 });
-
-app.get("/", async (req, res) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query("SELECT $1::text as message", [
-      "연결성공!",
-    ]);
-    const message = result.rows[0].message;
-    res.send(message);
-    client.release();
-  } catch (error) {
-    console.error("Error executing query", error);
-    res.status(500).send("Internal Server Error");
+app.post(
+  "/notes",
+  (req, res, next) => {
+    console.log("Middleware test");
+    next();
+  },
+  async (req, res) => {
+    console.log(res.body);
+    data.push(req.body);
+    res.sendStatus(201);
   }
-});
+);
 
 app.listen(port, function () {
   console.log(`Server is running on http://localhost:${port}`);
